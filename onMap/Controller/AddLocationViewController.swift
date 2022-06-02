1003b767faffb6d0858105fe762cdb929d20c2cc
Dirisu <dirisumarvellous94@gmail.com>
2022-06-02 16:41:04 +0100

    added the textfields delegate and locations functions for the addLocationConntroller,  updated the finishAddLocationController

--- onMap/Controller/AddLocationViewController.swift
+++ onMap/Controller/AddLocationViewController.swift
@@ -6,18 +6,148 @@
 //
 
 import UIKit
+import MapKit
 
-class AddLocationViewController: UIViewController {
+class AddLocationViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var websiteTextField: UITextField!
     @IBOutlet weak var findLocationButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    var objectId: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationTextField.delegate = self
+        websiteTextField.delegate = self
+//        buttonEnabled(false, button: findLocationButton)
+    }
+    
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func findLocation(_ sender: UIButton) {
+        let newLocation = locationTextField.text
+        
+        guard let url = URL(string: self.websiteTextField.text!), UIApplication.shared.canOpenURL(url) else {
+            self.showAlert(message: "Please include 'http://' in your website adddress", title: "URL NOT VALID")
+            return
+        }
 
+        geoCodePosition(newLocation: newLocation ?? "")
+    }
+    
+    private func geoCodePosition(newLocation: String) {
+        self.activityIndicator.startAnimating()
+        CLGeocoder().geocodeAddressString(newLocation) { (newMarker, error) in
+            if let error = error {
+                self.showAlert(message: error.localizedDescription, title: "Location Not Found")
+                self.setLoading(false)
+                print("Location not found.")
+            } else {
+                var location: CLLocation?
+                
+                if let marker = newMarker, marker.count > 0 {
+                    location = marker.first?.location
+                }
+                
+                if let location = location {
+                    self.loadNewLocation(location.coordinate)
+                } else {
+                    self.showAlert(message: "Please try again later.", title: "Error")
+                    self.setLoading(false)
+                    print("Error")
+                }
+            }
+        }
+    }
+    
+    // Push to Final Add Location screen
+    
+    private func loadNewLocation(_ coordinate: CLLocationCoordinate2D) {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "FinishAddLocationViewController") as! FinishAddLocationViewController
+        controller.studentInformation = buildStudentInfo(coordinate)
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    // Student info to be displayed on Final Add Location screen
+    
+    private func buildStudentInfo(_ coordinate: CLLocationCoordinate2D) -> StudentInformation {
+        
+        var studentInfo = [
+            "uniqueKey": UdacityClient.Auth.key,
+            "firstName": UdacityClient.Auth.firstName,
+            "lastName": UdacityClient.Auth.lastName,
+            "mapString": locationTextField.text!,
+            "mediaURL": websiteTextField.text!,
+            "latitude": coordinate.latitude,
+            "longitude": coordinate.longitude,
+            ] as [String: AnyObject]
+        
+        if let objectId = objectId {
+            studentInfo["objectId"] = objectId as AnyObject
+            print(objectId)
+        }
+
+        return StudentInformation(studentInfo)
+
+    }
+    
+    // handles activityView and Button actions
+    func setLoading(_ loading: Bool) {
+        if loading {
+            DispatchQueue.main.async {
+                self.activityIndicator.startAnimating()
+                self.buttonEnabled(false, button: self.findLocationButton)
+            }
+        } else {
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                self.buttonEnabled(true, button: self.findLocationButton)
+            }
+        }
+        DispatchQueue.main.async {
+            self.locationTextField.isEnabled = !loading
+            self.websiteTextField.isEnabled = !loading
+            self.findLocationButton.isEnabled = !loading
+        }
+    }
+    
+    
+    // text field delegates
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == locationTextField {
+            let currentText = locationTextField.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+            _ = currentText.replacingCharacters(in: stringRange, with: string)
+        }
+        
+        if textField == websiteTextField {
+            let currentText = websiteTextField.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+            _ = currentText.replacingCharacters(in: stringRange, with: string)
+        }
+        
+        return true
         
     }
     
-
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        buttonEnabled(false, button: findLocationButton)
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            
+        }
+        return true
+    }
 }
--- onMap/Controller/FinishAddLocationViewController.swift
+++ onMap/Controller/FinishAddLocationViewController.swift
@@ -8,18 +8,102 @@
 import UIKit
 import MapKit
 
-class FinishAddLocationViewController: UIViewController {
+class FinishAddLocationViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var studentInformation: StudentInformation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
 
-        // Do any additional setup after loading the view.
+        if let studentLocation = studentInformation {
+            let studentLocation = Location(
+                objectId: studentLocation.objectId ?? "",
+                uniqueKey: studentLocation.uniqueKey,
+                firstName: studentLocation.firstName,
+                lastName: studentLocation.lastName,
+                mapString: studentLocation.mapString,
+                mediaURL: studentLocation.mediaURL,
+                latitude: studentLocation.latitude,
+                longitude: studentLocation.longitude,
+                createdAt: studentLocation.createdAt ?? "",
+                updatedAt: studentLocation.updatedAt ?? ""
+            )
+            showLocations(location: studentLocation)
+        }
     }
     
-
+    // shows location cordinates
     
-
+    private func showLocations(location: Location) {
+        mapView.removeAnnotations(mapView.annotations)
+        if let coordinate = extractCoordinate(location: location) {
+            let annotation = MKPointAnnotation()
+            annotation.title = location.firstName
+            annotation.subtitle = location.mediaURL ?? ""
+            annotation.coordinate = coordinate
+            mapView.addAnnotation(annotation)
+            mapView.showAnnotations(mapView.annotations, animated: true)
+        }
+    }
+    
+    private func extractCoordinate(location: Location) -> CLLocationCoordinate2D? {
+        if let lat = location.latitude, let long = location.longitude {
+            return CLLocationCoordinate2DMake(lat, long)
+        }
+        return nil
+    }
+    
+    // updates final location to the mapview && || table
+    
+    @IBAction func submitPressed(_ sender: UIButton) {
+        if let studentLocation = studentInformation {
+            if UdacityClient.Auth.objectId == "" {
+                UdacityClient.addStudentLocation(information: studentLocation) { (success, error) in
+                    if success {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                            print("successfully updated")
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            self.showAlert(message: error?.localizedDescription ?? "", title: "Error")
+                            print("data not correctly parsed")
+                        }
+                    }
+                }
+                
+            } else {
+                let alertVC = UIAlertController(title: "", message: "This student has already posted a location. Would you like to overwrite this location?", preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction(title: "Overwrite", style: .default, handler: { (action: UIAlertAction) in
+                    UdacityClient.updateStudentLocation(information: studentLocation) { (success, error) in
+                        if success {
+                            DispatchQueue.main.async {
+                                self.dismiss(animated: true, completion: nil)
+                            }
+                        } else {
+                            DispatchQueue.main.async {
+                                self.showAlert(message: error?.localizedDescription ?? "", title: "Error")
+                            }
+                        }
+                    }
+                }))
+                
+                alertVC.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction) in
+                    DispatchQueue.main.async {
+                        alertVC.dismiss(animated: true, completion: nil)
+                    }
+                }))
+                
+                self.present(alertVC, animated: true)
+            }
+        }
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
 }
